1e1cbe3a6e6b769b7c4210ad731c0d72103bcf79
henia00 <henia00@gmail.com>
2024-02-20 12:10:11 +0100

    New html templates

--- pages/urls.py
+++ pages/urls.py
@@ -3,4 +3,9 @@ from pages import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('models/', views.models, name='models'),
+    path('tools/', views.tools, name='tools'),
+    path('papers/', views.papers, name='papers'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
 ]
