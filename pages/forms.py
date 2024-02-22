783467d8e9448dc555a2ec196430058b7204cdfd
henia00 <henia00@gmail.com>
2024-02-22 13:56:37 +0100

    Styling home page

--- pages/forms.py
+++ pages/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.forms import ModelForm
 from hadsdb.models import Dane
+from django.utils.safestring import mark_safe
 
 class DatabaseQueryForm(forms.Form):
     pass
@@ -9,4 +10,8 @@ class QueryHadsForm(ModelForm):
     class Meta:
         model = Dane
         fields = ['mass', 'feh', 'fov']
+        labels = {
+            "feh": "[Fe/H]",
+            "fov": mark_safe("f<sub>ov</sub>")
+        }
 
